02da258933fd2aa29fe6c94d1a59b86e10ad4dec
bartlomiejlorenowicz <145027905+bartlomiejlorenowicz@users.noreply.github.com>
2024-08-10 00:44:47 +0200

    Add validation to User and UserDto using annotations

--- src/main/java/com/quickweather/dto/UserDto.java
+++ src/main/java/com/quickweather/dto/UserDto.java
@@ -1,13 +1,25 @@
 package com.quickweather.dto;
 
-import java.util.Objects;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class UserDto {
-
+    @NotBlank(message = "First name is mandatory")
+    @Size(min = 2, max = 30, message = "first name must be between 2 and 30 characters")
     private String firstName;
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 30, message = "last name must be between 2 and 30 characters")
     private String lastName;
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "password must be minimum 8 characters long")
     private String password;
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "email must be valid")
     private String email;
+
+    @NotBlank(message = "Phone is mandatory")
+    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters")
     private String phoneNumber;
 
     public UserDto() {
