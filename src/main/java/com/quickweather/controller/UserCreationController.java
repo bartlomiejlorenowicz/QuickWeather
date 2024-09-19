14cd09191b9cc592e5fdc197df9638b6461c5c9d
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-09-19 22:16:40 +0200

    Refactor UserCreationController.java Changed return type of register method to ResponseEntity with success message

--- src/main/java/com/quickweather/controller/UserCreationController.java
+++ src/main/java/com/quickweather/controller/UserCreationController.java
@@ -2,23 +2,33 @@ package com.quickweather.controller;
 
 import com.quickweather.dto.UserDto;
 import com.quickweather.service.UserCreationService;
-import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user/register")
 public class UserCreationController {
 
-   private UserCreationService userCreationService;
+   private final UserCreationService userCreationService;
 
     public UserCreationController(UserCreationService userCreationService) {
         this.userCreationService = userCreationService;
     }
 
     @PostMapping
-    public void register(@RequestBody UserDto userDto) {
+    public ResponseEntity<Map<String, String>> register(@RequestBody UserDto userDto) {
         userCreationService.createUser(userDto);
-    }
 
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "User successfully registered");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response);
+    }
 
 }
