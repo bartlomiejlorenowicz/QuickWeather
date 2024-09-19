13662eb2d17d6e3b62b1960265b615a8cbfe6364
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-09-19 22:27:47 +0200

    Remove validation annotations from UserDto

--- src/main/java/com/quickweather/dto/UserDto.java
+++ src/main/java/com/quickweather/dto/UserDto.java
@@ -1,32 +1,18 @@
 package com.quickweather.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public class UserDto {
-    @NotBlank(message = "First name is mandatory")
-    @Size(min = 2, max = 30, message = "first name must be between 2 and 30 characters")
+
     private String firstName;
 
-    @NotBlank(message = "Last name is mandatory")
-    @Size(min = 2, max = 30, message = "last name must be between 2 and 30 characters")
     private String lastName;
 
-    @NotBlank(message = "Password is mandatory")
-    @Size(min = 8, message = "password must be minimum 8 characters long")
-    @Pattern(regexp = ".*[!@#$%^&*(),.?\\\":{}|<>].*")
     private String password;
 
-    @NotBlank(message = "Email is mandatory")
-    @Email(message = "email must be valid")
     private String email;
 
-    @NotBlank(message = "Phone is mandatory")
-    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 characters")
     private String phoneNumber;
 
     public UserDto() {
