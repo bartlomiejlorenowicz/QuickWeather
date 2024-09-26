386280c643bca2396483df81553668da2e8a19e1
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-09-26 19:17:48 +0200

    Refactor register method to return status code instead of response body

--- src/main/java/com/quickweather/controller/UserCreationController.java
+++ src/main/java/com/quickweather/controller/UserCreationController.java
@@ -2,33 +2,25 @@ package com.quickweather.controller;
 
 import com.quickweather.dto.UserDto;
 import com.quickweather.service.UserCreationService;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user/register")
 public class UserCreationController {
 
-   private final UserCreationService userCreationService;
+    private final UserCreationService userCreationService;
 
     public UserCreationController(UserCreationService userCreationService) {
         this.userCreationService = userCreationService;
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, String>> register(@RequestBody UserDto userDto) {
+    public ResponseEntity<Void> register(@RequestBody UserDto userDto) {
         userCreationService.createUser(userDto);
-
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "User successfully registered");
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(response);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
