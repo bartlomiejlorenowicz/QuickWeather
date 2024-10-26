446fc79044c8ddee5a375ab488aeebd6d76752d9
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-10-26 15:04:20 +0200

    Add WeatherService and Exception

--- src/main/java/com/quickweather/service/openweathermap/OpenWeatherServiceImpl.java
+++ src/main/java/com/quickweather/service/openweathermap/OpenWeatherServiceImpl.java
@@ -5,18 +5,20 @@ import com.quickweather.dto.forecast.HourlyForecastResponseDto;
 import com.quickweather.dto.forecast.WeatherForecastDailyResponseDto;
 import com.quickweather.dto.weather.WeatherResponse;
 import com.quickweather.dto.zipcode.WeatherByZipCodeResponseDto;
+import com.quickweather.service.weatherbase.WeatherServiceBase;
 import com.quickweather.utils.UriBuilderUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
-public class OpenWeatherServiceImpl implements OpenWeatherMapService {
+public class OpenWeatherServiceImpl extends WeatherServiceBase implements OpenWeatherMapService {
 
     @Value("${open.weather.api.key}")
     private String apiKey;
@@ -24,84 +26,63 @@ public class OpenWeatherServiceImpl implements OpenWeatherMapService {
     @Value("${open.weather.api.url}")
     private String apiUrl;
 
-    private final RestTemplate restTemplate;
-
     public OpenWeatherServiceImpl(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+        super(restTemplate);
     }
 
     @Override
     public WeatherResponse getCurrentWeatherByCity(String city) {
-        URI url = UriBuilderUtils.buildWeatherUri(apiUrl, apiKey, city);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("q", "city");
+        queryParams.put("appid", apiKey);
+        queryParams.put("unit", "metrics");
 
-        try {
-            return restTemplate.getForObject(url, WeatherResponse.class);
-        } catch (HttpClientErrorException e) {
-            log.error("HTTP error fetching weather data for {}: {}", city, e.getMessage());
-            throw new RuntimeException("Error fetching weather data for " + city);
-        } catch (Exception e) {
-            log.error("HTTP error fetching weather data for {}: {}", city, e.getMessage());
-            throw new RuntimeException("Could not fetch weather data for " + city);
-        }
+        URI url = UriBuilderUtils.buildUri(apiUrl, "weather", queryParams);
+        return fetchWeatherData(url, WeatherResponse.class, city);
     }
 
     @Override
     public WeatherByZipCodeResponseDto getCurrentWeatherByZipcode(String zipcode, String countryCode) {
-        URI url = UriBuilderUtils.buildZipCodeUri(apiUrl, apiKey, zipcode, countryCode);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("zip", zipcode + "," + countryCode);
+        queryParams.put("appid", apiKey);
+        queryParams.put("lang", "pl");
 
-        try {
-            return restTemplate.getForObject(url, WeatherByZipCodeResponseDto.class);
-        } catch (HttpClientErrorException e) {
-            log.error("HTTP error fetching country by zipcode {}", zipcode);
-            throw new RuntimeException("Error fetching country by zipcode " + zipcode);
-        } catch (Exception e) {
-            log.error("HTTP error fetching country by zipcode " + zipcode);
-            throw new RuntimeException("Could not fetch country by zipcode " + zipcode);
-        }
+        URI url = UriBuilderUtils.buildUri(apiUrl, "weather", queryParams);
+        return fetchWeatherData(url, WeatherByZipCodeResponseDto.class, zipcode + "," + countryCode);
     }
 
     @Override
     public HourlyForecastResponseDto get5DaysForecastEvery3Hours(String city) {
-        URI url = UriBuilderUtils.buildHourlyUri(apiUrl, apiKey, city);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("q", city);
+        queryParams.put("appid", apiKey);
+        queryParams.put("units", "metric");
+        queryParams.put("lang", "pl");
 
-        try {
-            return restTemplate.getForObject(url, HourlyForecastResponseDto.class);
-        } catch (HttpClientErrorException e) {
-            log.error("HTTP error fetching forecast for city {}", city);
-            throw new RuntimeException("Error fetching forecast for city: " + city);
-        } catch (Exception e) {
-            log.error("HTTP error fetching forecast for city: " + city);
-            throw new RuntimeException("Could not fetch forecast for city " + city);
-        }
+        URI url = UriBuilderUtils.buildUri(apiUrl, "forecast", queryParams);
+        return fetchWeatherData(url, HourlyForecastResponseDto.class, city);
     }
 
     @Override
     public AirPollutionResponseDto getAirPollutionByCoordinates(double lat, double lon) {
-        URI url = UriBuilderUtils.buildAirPollutionUri(apiUrl, apiKey, lat, lon);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("lat", String.valueOf(lat));
+        queryParams.put("lon", String.valueOf(lon));
+        queryParams.put("appid", apiKey);
 
-        try {
-            return restTemplate.getForObject(url, AirPollutionResponseDto.class);
-        } catch (HttpClientErrorException e) {
-            log.error("HTTP error fetching air pollution for lat and lon {}, {}", lat, lon);
-            throw new RuntimeException("Error fetching air pollution for lat and lon", e);
-        } catch (Exception e) {
-            log.error("HTTP error fetching for air pollution lat {}, lon {}", lat, lon);
-            throw new RuntimeException("Could not fetch air pollution for lat " + lat + " and lon " + lon, e);
-        }
+        URI url = UriBuilderUtils.buildUri(apiUrl, "air_pollution", queryParams);
+        return fetchWeatherData(url, AirPollutionResponseDto.class, lat + "," + lon);
     }
 
     @Override
     public WeatherForecastDailyResponseDto getWeatherForecastByCityAndDays(String city, int cnt) {
-        URI url = UriBuilderUtils.buildForecastDaily(apiUrl, apiKey, city, cnt);
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("q", city);
+        queryParams.put("appid", apiKey);
+        queryParams.put("cnt", String.valueOf(cnt));
 
-        try {
-            return restTemplate.getForObject(url, WeatherForecastDailyResponseDto.class);
-        } catch (HttpClientErrorException e) {
-            log.error("HTTP error fetching daily forecast for city {}, cnt {}", city, cnt);
-            throw new RuntimeException("error fetching daily forecast for city " + city + " cnt " + cnt);
-        } catch (Exception e) {
-            log.error("HTTP error fetching daily forecast for city {}, cnt {}", city, cnt);
-            throw new RuntimeException("Could not fetch daily forecast for city " + city + " cnt " + cnt, e);
-        }
+        URI url = UriBuilderUtils.buildUri(apiUrl, "forecast/daily", queryParams);
+        return fetchWeatherData(url, WeatherForecastDailyResponseDto.class, city + "," + cnt);
     }
 }
