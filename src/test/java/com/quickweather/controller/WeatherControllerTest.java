8854be74c09cf85ed60db73ae72cac1787039d71
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-11-03 21:51:02 +0100

    added unit and integration tests for WeatherController

--- src/test/java/com/quickweather/controller/WeatherControllerTest.java
+++ src/test/java/com/quickweather/controller/WeatherControllerTest.java
@@ -2,17 +2,26 @@ package com.quickweather.controller;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.quickweather.dto.zipcode.WeatherByZipCodeResponseDto;
+import com.quickweather.exceptions.WeatherErrorType;
+import com.quickweather.exceptions.WeatherServiceException;
+import com.quickweather.service.accuweather.AccuWeatherServiceImpl;
+import com.quickweather.service.openweathermap.OpenWeatherServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,8 +40,17 @@ class WeatherControllerTest {
     @Value("${open.weather.api.url}")
     private String weatherApiUrl;
 
+    @Mock
+    private OpenWeatherServiceImpl openWeatherService;
+
+    @Mock
+    private AccuWeatherServiceImpl accuWeatherService;
+
+    @InjectMocks
+    private WeatherController weatherController;
+
     @Test
-    void testGetCurrentWeather() throws Exception {
+    void testGetCurrentWeather_ShouldReturnCorrect() throws Exception {
         String responseBody = "{ \"main\": { \"temp\": 15.0 }, \"weather\": [{ \"description\": \"sunny\" }], \"name\": \"London\" }";
 
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/weather"))
@@ -52,5 +70,126 @@ class WeatherControllerTest {
                 .andExpect(jsonPath("$.weather[0].description").value("sunny"))
                 .andExpect(jsonPath("$.name").value("London"));
     }
+
+    @Test
+    void testGetCurrentWeather_ShouldReturnBadRequest_WhenCityIsBlank() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/city")
+                        .param("city", "")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("City name cannot be blank"));
+    }
+
+    @Test
+    void testGetCurrentWeather_ShouldReturnNotFound_WhenDataNotFound() throws Exception {
+        when(openWeatherService.getCurrentWeatherByCity("UnknownCity"))
+                .thenThrow(new WeatherServiceException(WeatherErrorType.DATA_NOT_FOUND, "Data not found for: UnknownCity"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/city")
+                .param("city", "UnknownCity")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Data not found for: UnknownCity"));
+    }
+
+    @Test
+    void testGetCurrentWeatherByZipcode_ShouldReturnOk_WhenValidRequest() throws Exception {
+        String responseBody = "{ \"sys\": { \"country\": \"PL\" }, \"weather\":[{ \"description\": \"clear sky\" }] }";
+
+        stubFor(WireMock.get(urlPathEqualTo("/data/2.5/weather"))
+                .withQueryParam("zip", equalTo("37-203,pl"))
+                .willReturn(aResponse()
+                        .withBody(responseBody)
+                        .withHeader("Content-Type", "application/json")
+                        .withStatus(200)));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/zipcode")
+                        .param("zipcode", "37-203")
+                        .param("countryCode", "pl")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sys.country").value("PL"))
+                .andExpect(jsonPath("$.weather[0].description").value("clear sky"));
+    }
+
+    @Test
+    void testGetCurrentWeatherByZipcode_ShouldReturnBadRequest_WhenCountryCodeIsInvalid() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/zipcode")
+                        .param("zipcode", "37-203")
+                        .param("countryCode", "1111")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Country  code must be 2 letters"));
+    }
+
+    @Test
+    void testGetCurrentWeatherByZipcode_ShouldReturnBadRequest_WhenZipcodeIsMissing() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/zipcode")
+                        .param("countryCode", "pl")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Required request parameter 'zipcode' is missing"));
+    }
+
+    @Test
+    void testGet5DaysForecast_ShouldReturnCorrect() throws Exception {
+        String weatherResponse = """
+{
+  "city": {
+    "id": 123,
+    "name": "Sample City",
+    "country": "PL"
+  },
+  "list": [
+    {
+      "dt": 1672459200,
+      "main": {
+        "temp": 15.0,
+        "feels_like": 14.0,
+        "temp_min": 13.5,
+        "temp_max": 16.0,
+        "pressure": 1012,
+        "humidity": 78
+      },
+      "weather": [
+        {
+          "main": "Clear",
+          "description": "clear sky",
+          "icon": "01d"
+        }
+      ],
+      "wind": {
+        "speed": 3.5,
+        "deg": 180
+      },
+      "clouds": {
+        "all": 10
+      },
+      "dt_txt": "2024-11-04 12:00:00"
+    }
+  ]
+}
+""";
+        stubFor(WireMock.get(urlPathEqualTo("/data/2.5/forecast"))
+                .withQueryParam("q", equalTo("London"))
+                .withQueryParam("appid", equalTo(apiKey))
+                .withQueryParam("units", equalTo("metric"))
+                .withQueryParam("lang", equalTo("pl"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(weatherResponse)
+                        .withStatus(200)));
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/weather/forecast")
+                        .param("city", "London")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.list[0].main.temp").value(15.0))
+                .andExpect(jsonPath("$.list[0].weather[0].description").value("clear sky"))
+                .andExpect(jsonPath("$.city.name").value("Sample City"));
+    }
+
+    //todo /air-quality
 
 }
