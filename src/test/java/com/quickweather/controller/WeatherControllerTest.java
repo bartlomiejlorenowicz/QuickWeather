6ef08bb190b9f8503bc8a8311d7799e68c934cb0
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-11-05 19:01:32 +0100

    - Added JSON files containing sample ResponseDto responses to `src/test/resources/app/responses`. - Updated test method names in `WeatherControllerTest` to be more descriptive and follow naming conventions.

--- src/test/java/com/quickweather/controller/WeatherControllerTest.java
+++ src/test/java/com/quickweather/controller/WeatherControllerTest.java
@@ -2,32 +2,25 @@ package com.quickweather.controller;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import com.quickweather.exceptions.WeatherErrorType;
-import com.quickweather.exceptions.WeatherServiceException;
-import com.quickweather.service.accuweather.AccuWeatherServiceImpl;
-import com.quickweather.service.openweathermap.OpenWeatherServiceImpl;
+import com.quickweather.validator.IntegrationTestConfig;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 @WireMockTest(httpPort = 8081)
 @ActiveProfiles("test")
-class WeatherControllerTest {
+class WeatherControllerTest extends IntegrationTestConfig {
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,20 +31,11 @@ class WeatherControllerTest {
     @Value("${open.weather.api.url}")
     private String weatherApiUrl;
 
-    @Mock
-    private OpenWeatherServiceImpl openWeatherService;
-
-    @Mock
-    private AccuWeatherServiceImpl accuWeatherService;
-
-    @InjectMocks
-    private WeatherController weatherController;
-
     private final String url = "/api/weather";
 
     @Test
-    void testGetCurrentWeather_ShouldReturnCorrect() throws Exception {
-        String responseBody = "{ \"main\": { \"temp\": 15.0 }, \"weather\": [{ \"description\": \"sunny\" }], \"name\": \"London\" }";
+    void shouldReturnWeatherData_WhenCityIsValid() throws Exception {
+        String responseBody = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/current_weather.json")));
 
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/weather"))
                 .withQueryParam("q", equalTo("London"))
@@ -72,7 +56,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetCurrentWeather_ShouldReturnBadRequest_WhenCityIsBlank() throws Exception {
+    void shouldReturnBadRequest_WhenCityIsBlank() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/city")
                         .param("city", "")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -81,10 +65,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetCurrentWeather_ShouldReturnNotFound_WhenDataNotFound() throws Exception {
-        when(openWeatherService.getCurrentWeatherByCity("UnknownCity"))
-                .thenThrow(new WeatherServiceException(WeatherErrorType.DATA_NOT_FOUND, "Data not found for: UnknownCity"));
-
+    void shouldReturnNotFound_WhenCityIsUnknown() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/city")
                         .param("city", "UnknownCity")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -93,8 +74,8 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetCurrentWeatherByZipcode_ShouldReturnOk_WhenValidRequest() throws Exception {
-        String responseBody = "{ \"sys\": { \"country\": \"PL\" }, \"weather\":[{ \"description\": \"clear sky\" }] }";
+    void shouldReturnWeatherData_WhenZipcodeIsValid() throws Exception {
+        String responseBody = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/current_weather_by_zipcode.json")));
 
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/weather"))
                 .withQueryParam("zip", equalTo("37-203,pl"))
@@ -113,7 +94,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetCurrentWeatherByZipcode_ShouldReturnBadRequest_WhenCountryCodeIsInvalid() throws Exception {
+    void shouldReturnBadRequest_WhenCountryCodeIsInvalid() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/zipcode")
                         .param("zipcode", "37-203")
                         .param("countryCode", "1111")
@@ -123,7 +104,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetCurrentWeatherByZipcode_ShouldReturnBadRequest_WhenZipcodeIsMissing() throws Exception {
+    void shouldReturnBadRequest_WhenZipcodeIsMissing() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/zipcode")
                         .param("countryCode", "pl")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -132,44 +113,9 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGet5DaysForecast_ShouldReturnCorrect() throws Exception {
-        String weatherResponse = """
-                {
-                  "city": {
-                    "id": 123,
-                    "name": "Sample City",
-                    "country": "PL"
-                  },
-                  "list": [
-                    {
-                      "dt": 1672459200,
-                      "main": {
-                        "temp": 15.0,
-                        "feels_like": 14.0,
-                        "temp_min": 13.5,
-                        "temp_max": 16.0,
-                        "pressure": 1012,
-                        "humidity": 78
-                      },
-                      "weather": [
-                        {
-                          "main": "Clear",
-                          "description": "clear sky",
-                          "icon": "01d"
-                        }
-                      ],
-                      "wind": {
-                        "speed": 3.5,
-                        "deg": 180
-                      },
-                      "clouds": {
-                        "all": 10
-                      },
-                      "dt_txt": "2024-11-04 12:00:00"
-                    }
-                  ]
-                }
-                """;
+    void shouldReturn5DaysForecast_WhenCityIsValid() throws Exception {
+        String weatherResponse = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/forecast_for_5_days.json")));
+
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/forecast"))
                 .withQueryParam("q", equalTo("London"))
                 .withQueryParam("appid", equalTo(apiKey))
@@ -179,7 +125,6 @@ class WeatherControllerTest {
                         .withHeader("Content-Type", "application/json")
                         .withBody(weatherResponse)
                         .withStatus(200)));
-
 
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/forecast")
                         .param("city", "London")
@@ -191,29 +136,8 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetAirPollutionByCoordinates_ShouldReturnCorrect() throws Exception {
-
-        String responseDto = """
-                {
-                    "list": [
-                        {
-                            "main": {
-                                "aqi": 2.0
-                            },
-                            "components": {
-                                "co": "220.3",
-                                "no": "0",
-                                "no2": "0.25",
-                                "o3": "77.96",
-                                "so2": "0.03",
-                                "pm2_5": "0.5",
-                                "pm10": "0.5",
-                                "nh3": "0.03"
-                            }
-                        }
-                    ]
-                }
-                """;
+    void shouldReturnAirPollutionData_WhenCoordinatesAreValid() throws Exception {
+        String responseDto = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/air_pollution_response.json")));
 
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/air_pollution"))
                 .withQueryParam("lat", equalTo("50.0"))
@@ -233,7 +157,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetAirPollutionByCoordinates_ShouldReturnBadRequest_WhenLatSizeIsLessThan2() throws Exception {
+    void shouldReturnBadRequest_WhenLatitudeIsEmpty() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/air-quality")
                         .param("lat", "")
                         .param("lon", "50.0")
@@ -243,7 +167,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetAirPollutionByCoordinates_ShouldReturnBadRequest_WhenLatIsLessThanMinus90() throws Exception {
+    void shouldReturnBadRequest_WhenLatitudeIsOutOfBounds() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/air-quality")
                         .param("lat", "-91")
                         .param("lon", "50.0")
@@ -253,7 +177,7 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetAirPollutionByCoordinates_ShouldReturnBadRequest_WhenLonIsGreaterThan180() throws Exception {
+    void shouldReturnBadRequest_WhenLongitudeIsOutOfBounds() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(url + "/air-quality")
                         .param("lat", "50.0")
                         .param("lon", "181")
@@ -263,57 +187,8 @@ class WeatherControllerTest {
     }
 
     @Test
-    void testGetWeatherForecastByCityAndDays_ShouldReturnCorrect() throws Exception {
-
-        String responseDto = """
-                 {
-                          "city": {
-                              "id": 4517009,
-                              "name": "London",
-                              "country": "US"
-                          },
-                          "list": [
-                              {
-                                  "temp": {
-                                      "day": 20.98,
-                                      "min": 18.5,
-                                      "max": 23.1,
-                                      "night": 17.2,
-                                      "eve": 19.3,
-                                      "morn": 16.8
-                                  },
-                                  "pressure": 1019,
-                                  "humidity": 51,
-                                  "weather": [
-                                      {
-                                          "main": "Clouds",
-                                          "description": "scattered clouds",
-                                          "icon": "03d"
-                                      }
-                                  ]
-                              },
-                              {
-                                  "temp": {
-                                      "day": 21.54,
-                                      "min": 19.2,
-                                      "max": 24.0,
-                                      "night": 18.3,
-                                      "eve": 20.0,
-                                      "morn": 17.5
-                                  },
-                                  "pressure": 1019,
-                                  "humidity": 52,
-                                  "weather": [
-                                      {
-                                          "main": "Clouds",
-                                          "description": "broken clouds",
-                                          "icon": "04d"
-                                      }
-                                  ]
-                              }
-                          ]
-                      }
-                """;
+    void shouldReturnForecastByCityAndDays_WhenParametersAreValid() throws Exception {
+        String responseDto = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/forecast_by_city_and_by_days.json")));
 
         stubFor(WireMock.get(urlPathEqualTo("/data/2.5/forecast"))
                 .withQueryParam("q", equalTo("London"))
