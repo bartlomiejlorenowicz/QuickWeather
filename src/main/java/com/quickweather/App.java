6c4ba7a2d2030cb66f916bbc08fdf181e0d2f5fb
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-11-24 00:56:59 +0100

    Create table for storing API history and implement cache for city.

--- src/main/java/com/quickweather/App.java
+++ src/main/java/com/quickweather/App.java
@@ -2,9 +2,10 @@ package com.quickweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication()
+@EnableCaching
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
