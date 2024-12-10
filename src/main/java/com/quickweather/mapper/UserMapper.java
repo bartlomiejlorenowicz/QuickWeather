8fbfc93ba0c1cbdce72787cdd08ec6f7ad1f6a5c
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2024-12-10 23:01:46 +0100

    Updated security configuration with JWT authentication and role-based access.

--- src/main/java/com/quickweather/mapper/UserMapper.java
+++ src/main/java/com/quickweather/mapper/UserMapper.java
@@ -1,12 +1,11 @@
 package com.quickweather.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quickweather.dto.user.Role;
 import com.quickweather.dto.user.UserDto;
 import com.quickweather.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Slf4j
 @Component
@@ -19,10 +18,33 @@ public class UserMapper {
     }
 
     public User toEntity(UserDto userDto) {
-        log.info("starting mapping user " + userDto.getEmail());
-        User user = objectMapper.convertValue(userDto, User.class);
-        user.setUuid(UUID.randomUUID());
-        return user;
+        log.info("Mapping UserDto to User entity for email: {}", userDto.getEmail());
+        return User.builder()
+                .firstName(userDto.getFirstName())
+                .lastName(userDto.getLastName())
+                .email(userDto.getEmail())
+                .password(userDto.getPassword())
+                .phoneNumber(userDto.getPhoneNumber())
+                .isEnabled(true)
+                .isLocked(userDto.isLocked())
+                .role(userDto.getRole() == null ? Role.USER : userDto.getRole())
+                .build();
+    }
+
+    public UserDto toDto(User user) {
+        log.info("Mapping User entity to UserDto for email: {}", user.getEmail());
+        return UserDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .phoneNumber(user.getPhoneNumber())
+                .isEnabled(user.isEnabled())
+                .isLocked(user.isLocked())
+                .role(user.getRole())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
     }
 
 }
