f2312fa949204bd42f69dca6d1fe155c29fb6aea
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2025-01-23 19:03:28 +0100

    Refactor CustomUserDetailsService, test UserAccountStatusControllerIntegrationTest.java

--- src/test/java/com/quickweather/controller/AccuWeatherControllerTest.java
+++ src/test/java/com/quickweather/controller/AccuWeatherControllerTest.java
@@ -2,6 +2,14 @@ package com.quickweather.controller;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.quickweather.entity.Role;
+import com.quickweather.entity.RoleType;
+import com.quickweather.entity.User;
+import com.quickweather.repository.RoleRepository;
+import com.quickweather.repository.UserRepository;
+import com.quickweather.security.JwtUtil;
+import com.quickweather.service.user.CustomUserDetails;
+import com.quickweather.service.user.CustomUserDetailsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +22,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,11 +45,42 @@ public class AccuWeatherControllerTest {
     @Value("${accuweather.api.url.test}")
     private String accuApiUrl;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userCreationRepository;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Test
     void shouldReturnLocationByPostalCode_WhenPostcodeIsValid() throws Exception {
         String responseBody = new String(Files.readAllBytes(Paths.get("src/test/resources/app/responses/location_by_postcode.json")));
         System.out.println("Stub URL: " + accuApiUrl);
         System.out.println("Stub API Key: " + accuApiKey);
+
+        User user = User.builder()
+                .firstName("adam")
+                .lastName("zentyk")
+                .isEnabled(true)
+                .email("adam@wp.pl")
+                .phoneNumber("45364534123")
+                .uuid(UUID.randomUUID())
+                .roles(Set.of(
+                        roleRepository.findByRoleType(RoleType.USER).orElseThrow()
+                ))
+                .build();
+
+        user = userCreationRepository.save(user);
+
+        CustomUserDetails customUserDetails = customUserDetailsService.createCustomUserDetails(user);
+
+        Map<String, Object> generateToken = jwtUtil.generateToken(customUserDetails, user.getUuid());
+        String token = (String) generateToken.get("token");
 
         stubFor(WireMock.get(urlPathEqualTo(accuApiUrl))
                 .withQueryParam("apikey", equalTo(accuApiKey))
@@ -53,6 +95,7 @@ public class AccuWeatherControllerTest {
                         .param("postcode","37-203")
                         .param("language", "en-us")
                         .param("offset", "1")
+                        .header("Authorization", "Bearer " + token)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].LocalizedName").value("Gniewczyna Lancucka"))
