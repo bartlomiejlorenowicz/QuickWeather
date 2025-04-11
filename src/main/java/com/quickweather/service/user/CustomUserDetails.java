8d9f52ff567beb38148632a829b487b9d441fc67
Bartlomiej Lorenowicz <bartlomiejlorenowicz@gmail.com>
2025-04-11 11:35:20 +0200

    Add integration tests for controllers

--- src/main/java/com/quickweather/service/user/CustomUserDetails.java
+++ src/main/java/com/quickweather/service/user/CustomUserDetails.java
@@ -2,6 +2,7 @@ package com.quickweather.service.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,6 +10,7 @@ import java.util.Collection;
 import java.util.UUID;
 @Getter
 @AllArgsConstructor
+@Setter
 public class CustomUserDetails implements UserDetails {
 
     private final Long userId;
--- src/test/java/com/quickweather/security/JwtTestUtil.java
+++ src/test/java/com/quickweather/security/JwtTestUtil.java
@@ -1,32 +1,41 @@
 package com.quickweather.security;
 
+import com.quickweather.domain.User;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 @Component
 @Profile("test")
 public class JwtTestUtil {
 
     private Key key;
+    private Key resetKey;
 
     @Value("${jwt.secret}")
     private String secret;
 
+    @Value("${jwt.reset-secret}")
+    private String resetSecret;
+
     @PostConstruct
     public void init() {
-        // Initialize the Key using the secret
-        key = Keys.hmacShaKeyFor(secret.getBytes());
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        key = Keys.hmacShaKeyFor(decodedKey);
+
+        byte[] decodedResetKey = Base64.getDecoder().decode(resetSecret);
+        resetKey = Keys.hmacShaKeyFor(decodedResetKey);
     }
 
     public String generateToken(String username, String role) {
@@ -36,8 +45,47 @@ public class JwtTestUtil {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 godzina
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String generateResetToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("userId", user.getId())
+                .claim("type", "reset-password")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 15 * 60 * 1000)) // 15 min ważności
+                .signWith(resetKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException("token validation failed");
+        }
+    }
+
+    public boolean validateResetToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(resetKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            throw new RuntimeException("reset token validation failed");
+        }
+    }
+
+    public Key getResetKey() {
+        return resetKey;
     }
 }
